56de0cec339a5f0b7026a799778da6e90c09b46f
Long Chen <longch1024@gmail.com>
2021-01-08 20:51:28 +0800

    add imp fields

--- lxh_prediction/config.py
+++ lxh_prediction/config.py
@@ -7,6 +7,7 @@ data_file = os.path.join(root, "data/processed_data_1118.4.csv")
 cat_fields = "lsex age occupt marriage3 living llivealone dwell paint radiatn chemic fixture nuclear intere upset sleep tired appeti loser focus fret suicd impact relitn hit satify culutrue wm prisch junsch sensch juncoll lmi lstroke lcvd ht ldiafamily lsmoking lsmoked lneversmoke ldrinking ldrinked lneverdrink weich weial weime1 weime0 weime3 weime99 etime breakd lunchd dinnerd brehome breeate brerest lunhome luneate lunrest dinhome dineate dinrest grae0 ptae0 poke0 befe0 chie0 fise0 vege0 frue0 juie0 egge0 mike0 beae0 frye0 juce0 sode0 cake0 brne0 saue0 fure0 cofe0 orge0 vite0 tea lntea lteai lwork highintenswork leisphysical lphysactive lvigorous lvigday lmiddle lmidday walk0 walkday lseat1a lseat2a lseat1day lseat2day lgoodday1 lbadday1 lusephone lgest lfchild lfboy lfgirl lmbigb lmbn lmmboy lmmgirl lmchild lmboy lmgirl lfbigb lfbn lfmboy lfmgirl lght lghbs lbrestfe lmenop hypertension".split()
 scalar_fields = "lvighour lvigtime lmidhour lmidtime walkhour lwalktime lseat1hou lseat2hou seattime ntime lgotosleep lgetup nigtime lusephy lbftime2 lbftime_sum ASBP ADBP Ahr weight2 height2 wc hc BMI WHR WHtR weight20new".split()
 # onehot_fields = "occupt living dwell paint radiatn chemic fixture nuclear intere upset sleep tired appeti loser focus fret suicd impact relitn hit satify weich weial tea lusephone".split()
+important_fields = "Ahr age WHR wc ADBP ASBP weight2 BMI ntime WHtR lgotosleep lvigtime lwork lmidtime lmidday hc lneverdrink lvigday walkhour orge0".split()
 onehot_fields = "".split()
 optional_fields = ["FPG"]
 
@@ -16,6 +17,8 @@ ch_fields = "age lsex ldiafamily lghbs ht ASBP wc BMI".split()
 feature_fields = {
     "without_FPG": cat_fields + scalar_fields,
     "with_FPG": cat_fields + scalar_fields + optional_fields,
+    "imp_wo_FPG": important_fields,
+    "imp_with_FPG": important_fields + optional_fields,
     "ADA": ada_fields,
     "CH": ch_fields,
     "ADA_FPG": ada_fields + optional_fields,
