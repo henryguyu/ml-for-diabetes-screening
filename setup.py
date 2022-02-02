64a2b74fdf984bf215b508b663fbf725dbd9eaad
Long Chen <longch1024@gmail.com>
2022-02-02 15:36:10 +0800

    update setup

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import find_packages, setup
 
 setup(
     name="lxh_prediction",
-    packages=find_packages(exclude=["data"]),
+    packages=find_packages(exclude=["data", "scripts", "experiments"]),
     install_requires=["lightgbm", "scikit-learn", "matplotlib", "torch"],
 )
